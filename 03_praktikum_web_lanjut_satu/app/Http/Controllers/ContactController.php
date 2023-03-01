b010f833944955db20d2628e309a8d93cffb5213
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-01 20:16:04 +0700

    Add ContactController

---
+++ 03_praktikum_web_lanjut_satu/app/Http/Controllers/ContactController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('contact');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $message = $request->input('message');
+
+        echo 'Name: ' . $name . '<br>';
+        echo 'Email: ' . $email . '<br>';
+        echo 'Message: ' . $message . '<br>';
+        echo '<h2> Thank you for your message! </h2>';
+        echo '<h2> Your message has been sent. </h2>';
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
