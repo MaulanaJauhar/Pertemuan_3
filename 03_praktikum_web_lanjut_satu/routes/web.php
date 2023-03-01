e41439d503515d2f819867ec3351a7c13b5b9084
MaulanaJauhar <jauharmaulana4546@gmail.com>
2023-03-01 20:18:04 +0700

    Add Route

--- 03_praktikum_web_lanjut_satu/routes/web.php
+++ 03_praktikum_web_lanjut_satu/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//No 1
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
 });
+
+//No 2
+Route::prefix('products')->group(function () {
+    Route::get('/marbel_edu_games', function () {
+        return view('products')
+            ->with('name', 'Marbel Edu Games');
+    });
+    Route::get('/marbel_and_friends_kids_games', function () {
+        return view('products')
+            ->with('name', 'Marbel and Friends Kids Games');
+    });
+    Route::get('/riri_story_books', function () {
+        return view('products')
+            ->with('name', 'Riri Story Books');
+    });
+    Route::get('/kolak_kids_songs', function () {
+        return view('products')
+            ->with('name', 'Kolak Kids Songs');
+    });
+});
+
+//No 3
+Route::get('/news/{title?}', function ($title = null) {
+    return view('news')
+        ->with('title', $title);
+});
+
+//No 4
+Route::prefix('program')->group(function () {
+    Route::get('/karir', function () {
+        return view('program')
+            ->with('name', 'Karir Polinema');
+    });
+    Route::get('/magang', function () {
+        return view('program')
+            ->with('name', 'Magang Polinema');
+    });
+    Route::get('/kunjungan_industri', function () {
+        return view('program')
+            ->with('name', 'Kunjungan Industri Polinema');
+    });
+});
+
+//No 5
+Route::get('/about_us', function () {
+    return view('about_us');
+});
+
+//No 6
+Route::resource('contact', ContactController::class)->only([
+    'index', 'store'
+]);
